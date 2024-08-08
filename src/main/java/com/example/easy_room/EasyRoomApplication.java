72411d495e917ad92c9cfe13b6e1c5ee2df5940b
almelo <algacyroalga@gmail.com>
2024-08-08 14:39:43 -0300

    Create spring boot project

---
+++ src/main/java/com/example/easy_room/EasyRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.easy_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRoomApplication.class, args);
+	}
+
+}
